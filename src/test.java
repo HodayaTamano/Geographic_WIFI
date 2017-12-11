088c92bca5a1e3a4337a58cdac85425ffe2ba48a
Leandrog <Leandrog@DESKTOP-KE215AE>
2017-12-11 18:51:24 +0200

    Test was marked as comment - for now we ignore it.

--- src/test.java
+++ src/test.java
@@ -1,19 +1,19 @@
-
-public class test {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Compare comp;
-		comp = new Compare(){
-			public boolean filtrated(Wifi a, Wifi b, Wifi c){//commencing value and ending value
-				//in this part we need to compare between the different fields
-				//by id = we compare using equals
-				//for range values we check that the value of a point is between the commence and ending
-				//chose the type you want to sort by
-				//the commencing and ending values will be put into empty wifi values
-			}
-		}
-		
-	}
-
-}
+//
+//public class test {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		Compare comp;
+//		comp = new Compare(){s
+//			public boolean filtrated(Wifi a, Wifi b, Wifi c){//commencing value and ending value
+//				//in this part we need to compare between the different fields
+//				//by id = we compare using equals
+//				//for range values we check that the value of a point is between the commence and ending
+//				//chose the type you want to sort by
+//				//the commencing and ending values will be put into empty wifi values
+//			}
+//		}
+//		
+//	}
+//
+//}
