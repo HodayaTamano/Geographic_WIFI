7a4b557908fec32ef216098ca1dea42dc546a9e7
HodayaTamano <hodayat377@gmail.com>
2017-12-16 19:05:10 +0200

    Update Algorithm1

--- src/Algorithms/Algorithm1.java
+++ src/Algorithms/Algorithm1.java
@@ -1,6 +1,27 @@
 package Algorithms;
 
+import java.util.ArrayList;
+
+import Wifi_Data.Wifi_Scan;
+
 public class Algorithm1 {
 
+	public static double [] Location (ArrayList<Wifi_Scan> scansByNum){
+		double [] w_sum = new double [3];//loc
+		//loc[0] - lat, loc[1] - lon, loc[2] - alt
+		//loc[0]=scansByNum.
+		double sumofweight=0;
+		for (int i=0; i<scansByNum.size(); i++){
+			double weight = 1 /Math.pow(scansByNum.get(i).getWifi(scansByNum.get(i)).getSignal(),2);
+			w_sum[0] += scansByNum.get(i).getLat()*weight;
+			w_sum[1] += scansByNum.get(i).getLon()*weight;
+			w_sum[2] += scansByNum.get(i).getAlt()*weight;
+			sumofweight += weight;
+		}
+		w_sum[0] = w_sum[0]/sumofweight;
+		w_sum[1] = w_sum[1]/sumofweight;
+		w_sum[2] = w_sum[2]/sumofweight;
+		return w_sum;
+	}
 
 }
